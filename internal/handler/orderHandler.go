99480e54c3de06be6a9c06ad11c4daf626d9d537
Thirawoot.P <152477483+Thirawoot-Put@users.noreply.github.com>
2024-09-24 16:49:04 +0700

    handler

---
+++ internal/handler/orderHandler.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Thirawoot-Put/event-ticketing/payment-service/internal/domain"
+	"github.com/Thirawoot-Put/event-ticketing/payment-service/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type OrderHandler struct {
+	orderService *service.OrderService
+}
+
+func NewOrderHandler(service *service.OrderService) *OrderHandler {
+	return &OrderHandler{orderService: service}
+}
+
+func (h *OrderHandler) CreateOrder(c *gin.Context) {
+	var order domain.OrderEntity
+	if err := c.ShouldBindJSON(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	if err := h.orderService.CreateOrder(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, order)
+}
+
+func (h *OrderHandler) UpdateOrder(c *gin.Context) {
+	var order domain.OrderEntity
+	if err := c.ShouldBindJSON(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	if err := h.orderService.UpdateOrder(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, order)
+}
+
+func (h *OrderHandler) GetOrderByUserId(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	order, err := h.orderService.GetOrderByUserId(uint(id))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, order)
+}
